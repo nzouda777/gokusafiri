42b06d06547a2ebc538ac5ac664b56312ad55207
nzouda777 <nzoudarodrigue@gmail.com>
2026-06-19 05:19:25 +0100

    feat: listed features almost done

--- resources/js/Components/SearchBar.tsx
+++ resources/js/Components/SearchBar.tsx
@@ -485,7 +485,7 @@ export default function SearchBar({ initialValues, sticky: _sticky }: Props) {
                 <div className="md:pl-[6px] md:shrink-0 px-[2px] pb-[2px] md:p-0">
                     <button
                         onClick={handleSearch}
-                        className="w-full md:w-auto md:size-[62px] h-[52px] bg-[#6e8c79] rounded-full
+                        className="w-full md:w-auto md:size-[62px] h-[52px] w-[52px] bg-[#6e8c79] rounded-full
                                    flex items-center justify-center gap-[8px]
                                    text-white hover:bg-[#5a7865] transition-colors"
                     >
